ae157e6d449ae41087fb9b3610a4e5dd604414cb
Serje <Serje.havandjian@gmail.com>
2022-05-02 08:57:18 -0400

    commit tree 85914013192334f540ff2b51a3b6157ec30525a0

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
+
 function addingEventListener() {
+    const input = document.getElementById("input")
+    input.addEventListener("click", clickAlert)
+    
+    function clickAlert(){
+        alert("I was clicked")
+    }
 }
+
+addingEventListener()
